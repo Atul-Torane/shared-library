24a0eb02d1938b22c9b83b14c877d838ac3c885c
Atul Torane <55852411+Atul-Torane@users.noreply.github.com>
2024-06-23 15:49:49 +0530

    Update deployFlaskApp.groovy

--- vars/deployFlaskApp.groovy
+++ vars/deployFlaskApp.groovy
@@ -3,9 +3,8 @@
 def call(Map pipelineParams) {
     def VENV_DIR = pipelineParams.VENV_DIR ?: 'venv'
     def deployScript = """
-        . ${VENV_DIR}/bin/activate
-        nohup gunicorn --bind 0.0.0.0:5000 wsgi:app &
-        sleep 60
+        python3 -m venv ${VENV_DIR}
+        source ${VENV_DIR}/bin/activate
     """
     sh deployScript
 }
